5daf5968cca93de1693f455922f1f2d468997313
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-07-05 16:29:38 +0200

    feat(exchange): add `Exchange` interface

---
+++ starknet_trades/exchange/base.py
@@ -0,0 +1,12 @@
+"""Exchange interface"""
+
+import abc
+import asyncio
+
+from core.types import Symbol
+
+
+class Exchange:
+    @abc.abstractmethod
+    async def subscribe_ticker(self, symbol: Symbol) -> asyncio.Queue:
+        pass
