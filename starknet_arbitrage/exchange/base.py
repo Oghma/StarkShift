6e1cb017e1f3caf698845025fc162bc33a3c4104
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-07-11 17:32:26 +0200

    feat(exchange): add `subscribe_wallet` to the interface

--- starknet_arbitrage/exchange/base.py
+++ starknet_arbitrage/exchange/base.py
@@ -2,6 +2,7 @@
 
 import abc
 import asyncio
+import typing
 
 from core.types import Symbol
 
@@ -9,4 +10,10 @@ from core.types import Symbol
 class Exchange:
     @abc.abstractmethod
     async def subscribe_ticker(self, symbol: Symbol, **_) -> asyncio.Queue:
+        pass
+
+    @abc.abstractmethod
+    async def subscribe_wallet(
+        self, symbol: typing.Optional[Symbol] = None
+    ) -> asyncio.Queue:
         pass
