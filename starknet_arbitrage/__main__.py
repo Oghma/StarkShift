7a2a0c0a73a97566e463648964005df82df30190
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-07-16 18:41:26 +0200

    chore(main): update `__main__`

--- starknet_arbitrage/__main__.py
+++ starknet_arbitrage/__main__.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import os
 
+from decimal import Decimal
+
 from dotenv import dotenv_values
 from rich.logging import RichHandler
 
@@ -25,15 +27,18 @@ class Config:
         "QUOTE_ADDR",
         "QUOTE_DECIMALS",
         "QUOTE",
-        "SWAP_AMOUNT",
         "API_KEY",
         "SECRET_KEY",
+        "SIGNER_KEY",
+        "NODE_URL",
+        "SPREAD_THRESHOLD",
+        "MAX_AMOUNT_TRADE",
     }
 
     def __init__(self, config: dict) -> None:
         for key in self.REQUIRED_KEYS:
             if key not in config:
-                raise ValidationError(f"Missing `key`")
+                raise ValidationError(f"Missing `{key}`")
 
         self.base = Token(
             config["BASE"], config["BASE_ADDR"], int(config["BASE_DECIMALS"])
@@ -42,25 +47,48 @@ class Config:
             config["QUOTE"], config["QUOTE_ADDR"], int(config["QUOTE_DECIMALS"])
         )
         self.symbol = Symbol(self.base, self.quote)
-        self.amount = int(config["SWAP_AMOUNT"])
 
         self.api_key = config["API_KEY"]
         self.secret_key = config["SECRET_KEY"]
 
+        self.node_url = config["NODE_URL"]
+        self.account_address = config["ACCOUNT_ADDRESS"]
+        self.signer_key = config["SIGNER_KEY"]
+
+        self.spread_threshold = Decimal(config["SPREAD_THRESHOLD"])
+        self.max_amount_trade = Decimal(config["MAX_AMOUNT_TRADE"])
+        self.min_amount_trade = Decimal(config["MIN_AMOUNT_TRADE"])
+
 
 async def main():
+    # Bot settings
     logger = logging.getLogger("bot")
     logger.setLevel(logging.DEBUG)
     logger.addHandler(RichHandler())
-
+    # Load config
     config = {**dotenv_values(".env"), **os.environ}
     config = Config(config)
 
-    binance = Binance()
-    avnu = AVNU()
+    # Exchange initialisation
+    logger.debug("Connecting to starknet...")
+    chain = Starknet(config.node_url)
+    account = chain.get_account(config.account_address, config.signer_key)
+    logger.debug("Connecting to AVNU...")
+    avnu = AVNU(account)
 
-    monitor = SpreadMonitor([binance, avnu], config.symbol, config)
-    await monitor.run()
+    logger.debug("Connecting to binance...")
+    binance = Binance(config.api_key, config.secret_key)
+
+    # Run bot
+    logger.debug("All setup, running bot...")
+    bot = Arbitrage(
+        [binance, avnu],
+        config.symbol,
+        config.spread_threshold,
+        config.max_amount_trade,
+        config.min_amount_trade,
+    )
+    await bot.run()
 
 
 asyncio.run(main())
